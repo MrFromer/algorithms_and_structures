8530a2783de0be7d10f48ec515c93b997b102f7a
MrFromer <artem.safren.123@gmail.com>
2025-02-17 21:56:28 +0700

    Решил вторую задачу из первого контеста

--- Contest_1.py
+++ Contest_1.py
@@ -34,21 +34,28 @@ arr_check = [int(x) for x in input().split()]
 
 def binary_check(n,arr,target):
     left, right = 0, n-1
+    best_index = 0
     while left <= right:
         mid = (left+right)//2
         if arr[mid] == target:
-            return True
-        elif arr[mid] < target:
+            return arr[mid]
+        if abs(arr[mid]-target) < abs(arr[best_index]-target) or abs(arr[mid]-target) == abs(arr[best_index]-target) and arr[mid] < arr[best_index]:
+            best_index = mid
+
+        if arr[mid] < target:
             left = mid + 1
-        elif arr[mid] > target:
+        else:
             right = mid - 1
 
-    return False
+    return arr[best_index]
 
 for i in range(k):
-    if binary_check(n,arr,arr_check[i]) == True:
+    print(binary_check(n,arr,arr_check[i]))
 
-        print('YES')
-    elif binary_check(n,arr,arr_check[i]) == False:
 
-        print('NO')
+# 5 5
+# 1 3 5 7 9
+# 2 4 8 1 6
+
+
+
