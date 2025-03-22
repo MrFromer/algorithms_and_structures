07672dcf4bf5798cd4383c76f8231fceba66ff9c
MrFromer <artem.safren.123@gmail.com>
2025-03-22 18:28:34 +0700

    Решил задачу 5.2 из 5 контеста на KMP преф. функция

--- Contest_5/testing.py
+++ Contest_5/testing.py
@@ -1,48 +1,52 @@
-import sys
+t = input().strip()
+q = int(input())
+n = len(t)
 
-s = input().strip()
-n = len(s)
-m = int(input())
-k = 31  # Простое число для уменьшения коллизий
-MOD = 10**18 + 3  # Большое простое число
+def prefix_function(s):
+    """Корректно вычисляем префиксную функцию для строки s"""
+    m = len(s)
+    pi = [0] * m
+    j = 0
+    for i in range(1, m):  # Исправлено: обрабатываем все символы строки s
+        while j > 0 and s[i] != s[j]:
+            j = pi[j-1]
+        if s[i] == s[j]:
+            j += 1
+        pi[i] = j
+    return pi
 
-requests = []
-for _ in range(m):
-    requests.append(list(map(int, sys.stdin.readline().split())))
-
-# Словарь для индексов букв
-alfavit = {chr(i): i - ord('a') + 1 for i in range(ord('a'), ord('z') + 1)}
-
-# Предвычисление степеней k и префиксных хэшей
-prefix = [0] * (n + 1)
-power = [1] * (n + 1)
-for i in range(n):
-    power[i+1] = (power[i] * k) % MOD
-    prefix[i+1] = (prefix[i] + alfavit[s[i]] * power[i]) % MOD
-
-# Обработка запросов
-for req in requests:
-    a, b, c, d = req
-    # Проверка длины подстрок
-    if (b - a) != (d - c):
-        print("No")
+for _ in range(q):
+    s = input().strip()
+    m = len(s)
+    
+    # Обработка случая, когда подстрока длиннее текста
+    if m > n:
+        print(0)
         continue
     
-    a_start = a - 1
-    a_end = b
-    c_start = c - 1
-    c_end = d
+    # Случай, когда подстрока равна тексту
+    if m == n:
+        if s == t:
+            print(1, 0)
+        else:
+            print(0)
+        continue
     
-    # Вычисляем хэши
-    hash_a = (prefix[a_end] - prefix[a_start]) % MOD
-    hash_c = (prefix[c_end] - prefix[c_start]) % MOD
+    # Основной алгоритм KMP для m < n
+    pi = prefix_function(s)
+    occurrences = []
+    j = 0
     
-    # Корректируем степени
-    if a_start < c_start:
-        shift = c_start - a_start
-        hash_a = (hash_a * power[shift]) % MOD
-    else:
-        shift = a_start - c_start
-        hash_c = (hash_c * power[shift]) % MOD
+    for i in range(n):  # Исправлено: обрабатываем все символы текста t
+        while j > 0 and t[i] != s[j]:
+            j = pi[j-1]
+        if t[i] == s[j]:
+            j += 1
+        if j == m:
+            occurrences.append(i - m + 1)
+            j = pi[j-1]  # Сдвигаемся для поиска следующих вхождений
     
-    print("Yes" if hash_a == hash_c else "No")
+    print(len(occurrences), end='')
+    if occurrences:
+        print(' ' + ' '.join(map(str, occurrences)), end='')
+    print()
